325728f1ebf522b05db83249693b4fccaabf9487
Seth Callister <seth.callister3@gmail.com>
2024-04-24 15:20:52 -0600

    initial upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const hb = document.querySelector('#hamburgerBtn');
+const pn = document.querySelector('#primaryNav');
+
+hb.addEventListener('click', () => {
+    hb.classList.toggle('open');
+    pn.classList.toggle('open');
+});
